395856e67d3793971a16683b3ad6650d60568935
Yashikab <yashikaba@gmail.com>
2020-06-22 08:56:50 +0900

    update

--- projects/myreviewdog/src/token2env.sh
+++ projects/myreviewdog/src/token2env.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python src/get_token.py
-export REVIEWDOG_GITHUB_API_TOKEN=`cat src/token.conf`
-rm src/token.conf
+python /src/get_token.py
+export REVIEWDOG_GITHUB_API_TOKEN=`cat /src/token.conf`
+rm /src/token.conf
